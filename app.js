3e58ffe5f6612cf66d9a7571db9356ce77d24968
Dicsylvi <dicsylvi@gmail.com>
2024-05-22 11:37:58 -0400

    added new code + GH Actions

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
+//Subjecting this to  github actions
 require("./db/db.js");
 const mongoose = require("mongoose");
-const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-const cors = require('cors');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
-const fileUpload = require('express-fileupload');
+const express = require("express");
+const path = require("path");
+const favicon = require("serve-favicon");
+const cors = require("cors");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
 
-const index = require('./routes/index');
-const users = require('./routes/users');
+const index = require("./routes/index");
+const users = require("./routes/users");
 const projects = require("./routes/projects");
 const expenses = require("./routes/expenses");
 const helmet = require("helmet");
@@ -19,23 +20,25 @@ const helmet = require("helmet");
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(fileUpload());
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(helmet({
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  helmet({
     frameguard: {
-        action: "deny"
-    }
-}));
+      action: "deny",
+    },
+  })
+);
 // var node_ip = process.env.NODE_IP || 'http://localhost:3000';
 // var ssrf_url = node_ip+'/expenses/ssrf/'
 
@@ -46,29 +49,27 @@ app.use(helmet({
 //     }
 //  }));
 
-app.use('/', index);
-app.use('/users', users);
-app.use('/projects', projects);
-app.use('/expenses', expenses);
-
-
+app.use("/", index);
+app.use("/users", users);
+app.use("/projects", projects);
+app.use("/expenses", expenses);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  let err = new Error('Not Found');
+app.use(function (req, res, next) {
+  let err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
